42cdb8861c727e7aa5ffd7b46dcc41d5212a64a4
Dongmin An <flaemtail@gmail.com>
2017-05-31 00:21:12 +0900

    Edited decomp.c to hold code ready for change later. test.c only changed for testing purpose.

--- Q2/tsp/decomp.c
+++ Q2/tsp/decomp.c
@@ -1,10 +1,28 @@
+/*
+Decomp.c is the solution to Quest 2 of lab 7.
+Written by Dong min An
+@flaemtail@gmail.com
+05/30/17
+
+Program is designed to recieve a list of gzipped files from stdin,
+then create multiple concurrent processes via fork that executes gzip
+to decompress each file in the given list.
+The program continues to terminate all child processes created 
+in order to ensue no zombies were created.
+
+tsp.tar is given file for demonstration.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
+#include "zlib.h"
+#include <string.h>
 
 #define BUFFERSIZE 1000
+#define LENGTH 0x1000
 
 void unix_error(char *msg)
 {
@@ -14,7 +32,7 @@ void unix_error(char *msg)
         exit(0);
 }
 
-pid_t Fork(void)
+pid_t Fork(void)	//Wrapper function to call error for Fork
 {
 
         pid_t pid;
@@ -23,22 +41,64 @@ pid_t Fork(void)
                 unix_error("Fork error");
         return pid;
 }
+void decomp(char* text)
+{
+	char* file_name = text;
+	gzFile * file;
+    	file = gzopen (file_name, "r");
+    	if (! file) 
+	{
+        	fprintf (stderr, "gzopen of '%s' failed: %s.\n", file_name,
+			strerror (errno));
+            	exit (EXIT_FAILURE);
+    	}
+    	while (1) 
+	{
+        	int err;                    
+	        int bytes_read;
+	        unsigned char buffer[LENGTH];
+       		bytes_read = gzread (file, buffer, LENGTH - 1);
+        	buffer[bytes_read] = '\0';
+        	printf ("%s", buffer);
+        	if (bytes_read < LENGTH - 1) 
+		{
+            		if (gzeof (file)) 
+			{
+                		break;
+            		}
+            	else 
+		{
+                	const char * error_string;
+                	error_string = gzerror (file, & err);
+                	if (err) 
+			{
+                    	fprintf (stderr, "Error: %s.\n", error_string);
+                    	exit (EXIT_FAILURE);
+                	}
+            	}
+        	}
+    }
+    gzclose (file);
+
+}
 
 int main(int argc, char *argv[])
 {
-	if(argc > 1)
+	if(argc > 1)	//Checks whether if there is at least one argument
 	{
-		char const* const file_n = argv[1];	
+		char const* const file_n = argv[1];	//filename is first argument
 		FILE* fp;
 		char buffer[BUFFERSIZE];
     		fp = fopen(argv[1], "r");
+		int lcounter = 0;	//loop counter that keeps track of # of childs
+
 		while (fgets(buffer, sizeof(buffer), fp) != NULL)
 		{
-			printf("%s | \n", buffer);
+			printf("%s", buffer);
 		}	 
         	
 	}
-	else
+	else	//if there is no argument, return an error
 	{
 		fprintf(stderr, "Argument not given\n");
 	}	
--- Q2/tsp/test.c
+++ Q2/tsp/test.c
@@ -1,22 +1,8 @@
-#include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
 
-int main()
+int main(void)
 {
-   char str[80] = "This is - www.tutorialspoint.com - website";
-   const char s[2] = "-";
-   char *token;
-   
-   /* get the first token */
-   token = strtok(str, s);
-   
-   /* walk through other tokens */
-   while( token != NULL ) 
-   {
-      printf( " %s\n", token );
-    
-      token = strtok(NULL, s);
-   }
-   
-   return(0);
-}
+	return execl ("/bin/pwd", "pwd", NULL);
+}	
