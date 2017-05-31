3efd790cc4e1ee02865539b03e5770e1d7b7c315
Dongmin An <flaemtail@gmail.com>
2017-06-01 02:11:41 +0900

    Fixed list.txt input. Can't seem to fix line 73, where gunzip will return an error 'No such file or directory'

--- Q2/tsp/decomp.c
+++ Q2/tsp/decomp.c
@@ -20,7 +20,7 @@ tsp.tar is given file for demonstration.
 #include <errno.h>
 #include <string.h>
 
-#define BUFFERSIZE 1000
+#define BUFFERSIZE 141
 
 void unix_error(char *msg)
 {
@@ -42,19 +42,46 @@ pid_t Fork(void)	//Wrapper function to call error for Fork
 
 int main(int argc, char *argv[])
 {
+
 	if(argc > 1)	//Checks whether if there is at least one argument
 	{
 		char const* const file_n = argv[1];	//filename is first argument
 		FILE* fp;
-		char buffer[BUFFERSIZE];
+		char c_buffer[BUFFERSIZE], *buffer[BUFFERSIZE];
     		fp = fopen(argv[1], "r");
 		int lcounter = 0;	//loop counter that keeps track of # of childs
+		int i, row, col;
+		
+		    /* Initializing array */
+    		for (row = 0; row < BUFFERSIZE; row++) 
+		{
+        	    	buffer[row] = "  ";
+        		
+		}
 
+		/* Reading file into array */
+		for (row = 0; row < BUFFERSIZE; row++) 
+		{
+       			if (fgets(c_buffer, BUFFERSIZE, fp))
+          			buffer[row] = strdup(c_buffer);
+    		}
+
+		/* Printing array */
+    		for (row = 0; row < BUFFERSIZE; row++) 
+		{
+            		printf("%s", buffer[row]);
+			execlp("gunzip", "gunzip", buffer[row], NULL);
+    		}		
+/*
 		while (fgets(buffer, sizeof(buffer), fp) != NULL)
 		{
+			//char str[80];
 			printf("%s", buffer);
-		}	 
-        	
+			//sprintf(str, "/home/s21600397/OSS_21600397/Q2/tsp/%s", buffer); 
+			execlp("gunzip", "gunzip", buffer, NULL);
+			lcounter++;
+		}*/	 
+        	fclose(fp);
 	}
 	else	//if there is no argument, return an error
 	{
